8b2b98082bf60ca680107fd8db60fabdc73c9274
kevin11h <kevhung11@gmail.com>
2015-02-22 17:54:21 -0800

    1st commit

---
+++ hw2/hw2.R
@@ -0,0 +1,6 @@
+## http://www.r-tutor.com/elementary-statistics/Interval-estimation/interval-estimate-popluation-proportion
+## [Scenario 1:] Begin p=by providing an estimate for the fraction of students who played a
+# video game in the week prior to the survey. Provide an interval estiamte as well as a point estimate
+# for this proportion.
+
+videodata = read.table("videodata.txt", header=TRUE)
